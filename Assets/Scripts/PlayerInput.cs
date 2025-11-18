c89e6e3209917a06b738c468fe77d1d8879852be
Y.Ouallal <2235702@talnet.nl>
2025-11-18 11:54:22 +0100

    PlayerInput

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,42 @@
+using System.Drawing;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField]private float _speed = 5;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            print("Ik heb W ingedrukt.");
+            Vector3 position = new Vector3(0, 0, 0);
+            transform.position += (new Vector3(0, 1, -1) * Time.deltaTime * _speed);
+            //  transform.rotation = quaternion.Euler(0, 0, 0);
+            //  transform.localScale = new Vector3(0, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            print("Ik heb A ingedrukt");
+            Vector3 position = new Vector3(0, 0, 0);
+            transform.position += (new Vector3(-1, 0, -1) * Time.deltaTime * _speed);
+            //   transform.rotation = quaternion.Euler(0, 0, 0);
+            //   transform.localScale = new Vector3(0, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Ik heb D ingedrukt.");
+            Vector3 position = new Vector3(0, 0, 0);
+            transform.position += (new Vector3(1, 0, -1) * Time.deltaTime * _speed);
+            //   transform.rotation = quaternion.Euler(0, 0, 0);
+            //   transform.localScale = new Vector3(0, 0, 0);
+        }
+    }
+}
