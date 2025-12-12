6173a53e62e6d28a1f91f5e8b55974efa3ff6633
Y.Ouallal <2235702@talnet.nl>
2025-12-12 10:49:42 +0100

    2D Platformer Update

--- Assets/CoinSpawner.cs
+++ Assets/CoinSpawner.cs
@@ -1,10 +1,13 @@
+using NUnit.Framework.Constraints;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject _coinPrefab;
+    [SerializeField] private GameObject _bigCoinPrefab;
     [SerializeField] private List<Transform> _coinSpawnLocations = new List<Transform>();
+    [SerializeField] private List<Transform> _bigCoinSpawnLocations = new List<Transform>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,6 +15,11 @@ public class CoinSpawner : MonoBehaviour
         foreach (Transform location in _coinSpawnLocations)
         {
             Instantiate(_coinPrefab, location.position, location.rotation);
+        }
+
+        foreach (Transform location in _bigCoinSpawnLocations)
+        {
+            Instantiate(_bigCoinPrefab, location.position, location.rotation);
         }
     }
 
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -2,13 +2,82 @@ using System;
 using System.Drawing;
 using Unity.Mathematics;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 public class PlayerInput : MonoBehaviour
 {
-    private float horizontal;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpingPower = 16f;
+
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private Vector2 movement;
+
+    private Vector2 screenBounds;
+
+    private float playerHalfWidth;
+
+    private float xPosLastFrame;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Animator animator;
+
+    private void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        playerHalfWidth = spriteRenderer.bounds.extents.x;
+
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        HandleMovement();
+        ClampMovement();
+        Flip();
+    }
+
+    private void Flip()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        if (input > 0 && (transform.position.x > xPosLastFrame))
+        {
+            spriteRenderer.flipX = false;
+        } else if (input < 0 && (transform.position.x < xPosLastFrame))
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        xPosLastFrame = transform.position.x;
+    }
+    private void ClampMovement()
+    {
+        float clampedX = Mathf.Clamp(transform.position.x, -screenBounds.x + playerHalfWidth, screenBounds.x - playerHalfWidth);
+        Vector2 position = transform.position;
+        position.x = clampedX;
+        transform.position = position;
+    }
+
+    private void HandleMovement()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        movement.x = input * speed * Time.deltaTime;
+        transform.Translate(movement);
+        
+        if (input != 0)
+        {
+            animator.SetBool("isRunning", true);
+        } else
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
+/*
+private float horizontal;
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
-    private bool isGrounded = false;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -22,22 +91,31 @@ public class PlayerInput : MonoBehaviour
 
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal") * speed;
+        horizontal = Input.GetAxisRaw("Horizontal");
 
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        animator.SetBool("isGrounded", IsGrounded());
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            animator.SetTrigger("Jump");
+        }
 
         if (horizontal != 0)
         {
             animator.SetBool("isRunning", true);
         }
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump"))
         {
             rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpingPower);
-            isGrounded = false;
-            animator.SetBool("isGrounded", !isGrounded);
-            animator.SetBool("isRunning", false);
-}
+            animator.SetBool("isGrounded", false);
+        }
+        else
+        {
+            animator.SetBool("isGrounded", true);
+        }
 
         if (Input.GetButtonUp("Jump") && rb.linearVelocityY > 0f)
         {
@@ -52,6 +130,16 @@ public class PlayerInput : MonoBehaviour
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocityY);
     }
 
+    public void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpingPower);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
     private void Flip()
     {
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
@@ -62,60 +150,4 @@ public class PlayerInput : MonoBehaviour
             transform.localScale = ls;
         }
     }
-}
-
-/*      float x = 0;
-        float y = 0;
-        //float z = 0;
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            print("Ik heb LEFT ingedrukt.");
-            _ = new Vector3(x = (_Xspeed), y, 0);
-            transform.position -= new Vector3(x, y, 0);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            print("Ik heb RIGHT ingedrukt.");
-            _ = new Vector3(x = (_Xspeed), y, 0);
-            transform.position += new Vector3(x, y, 0);
-        }
-         if (Input.GetKey(KeyCode.UpArrow))
-        {
-            print("Ik heb UP ingedrukt.");
-            _ = new Vector3(x, y = (_Yspeed), 0);
-            transform.position += new Vector3(x, y, 0);
-        } 
-
- [SerializeField] private float _Xspeed = 0.025f;
-    [SerializeField] private float _Yspeed = 0.075f;
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float X = 0;
-        float Y = 0;
-        //float Z = -1;
-        
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            print("Ik heb LeftArrow ingedrukt");
-            Vector3 position = new Vector3(X = (_Xspeed), Y, 0);
-            transform.position -= position;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            print("Ik heb RightArrow ingedrukt");
-            Vector3 position = new Vector3(X = (_Xspeed), Y, 0);
-            transform.position += position;
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            print("Ik heb UpArrow ingedrukt");
-            Vector3 position = new Vector3(X, Y = (_Yspeed), 0);
-            transform.position += position;
-        } */
+*/
